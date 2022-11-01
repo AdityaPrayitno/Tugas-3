090da376560863f7294f1470e642500f34fa5f95
AdityaPrayitno <72022338+AdityaPrayitno@users.noreply.github.com>
2022-11-02 03:15:14 +0700

    Add files via upload

---
+++ logo.php
@@ -0,0 +1,5 @@
+<div align="center">
+  <p><a href="datamahasiswa.php"><img src="LOGO-UNIGA-ONLY.png" width="470" height="490" border="0" /></a></p>
+  <p><h1><strong>ADITYA PRAYITNO</strong></h1></p>
+  <p><h2><strong>17510021</strong></h2></p>
+</div>
